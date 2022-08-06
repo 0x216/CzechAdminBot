841e7d3eed0b1d7c1b1fd43cc996d4eba4bce011
Jackie_boah <VanoGalen@yandex.ru>
2022-08-06 14:33:29 +0300

    third commit

--- config.py
+++ config.py
@@ -5,14 +5,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = '5568460231:AAGBUkv23kYL2n1E8aFxE0-1KE0VGLkJBAY'
-USER = 'postgres'
-PASSWORD = 'Loverdr2'
 
-# BOT_TOKEN = os.getenv('BOT_TOKEN')
-# CHAT_ID = os.getenv('CHAT_ID')
-# USER = os.getenv('USER')
-# PASSWORD = os.getenv("PASSWORD")
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
+USER = os.getenv('USER')
+PASSWORD = os.getenv("PASSWORD")
 
 ADMINS = []
 FORBIDDEN_WORDS = [
